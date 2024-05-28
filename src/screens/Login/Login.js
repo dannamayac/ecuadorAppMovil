4315ccb48602ed34a48a5dcc8dbc6a19bba33cb1
saorsu13 <sortizs@uqvirtual.edu.co>
2024-05-27 22:48:02 -0500

    Implementacion del login con backend

--- src/screens/Login/Login.js
+++ src/screens/Login/Login.js
@@ -1,24 +1,45 @@
-// LoginScreen.js
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, Image } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, Image, Alert } from 'react-native';
 import LoginStyles from '../../styles/LoginStyles';
 import { GlobalStyles } from '../../styles/GlobalStyles';
+import { REACT_APP_API_BASE_URL, REACT_APP_LOGIN_ENDPOINT } from '@env';
 
 const Login = ({ navigation }) => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleLoginPress = () => {
-    navigation.navigate('Authentication');
+  const handleLoginPress = async () => {
+    try {
+      const response = await fetch(`${REACT_APP_API_BASE_URL}${REACT_APP_LOGIN_ENDPOINT}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: username,
+          password: password,
+        }),
+      });
+
+      const result = await response.json();
+      if (result.status === 200) {
+        Alert.alert('Éxito', result.message);
+        navigation.navigate('Authentication');
+      } else {
+        Alert.alert('Error', result.message);
+      }
+    } catch (error) {
+      Alert.alert('Error', 'Ha ocurrido un error al iniciar sesión');
+    }
   };
 
   return (
     <View style={LoginStyles.container}>
       <View style={LoginStyles.logoContainer}>
-      <Image
+        <Image
           source={require('../../assets/Group 239046.png')}
           style={LoginStyles.logo}
-      />
+        />
       </View>
       <Text style={LoginStyles.title}>Hola, bienvenido al ingreso</Text>
       <Text style={LoginStyles.subTitle}>Usuario</Text>
